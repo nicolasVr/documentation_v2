26ad407a1a1cdd985aed8878ee7b4aebcfb2bb79
Axel <chabran.axel@gmail.com>
2024-02-09 18:00:12 +0100

    trad fr

--- i18n/fr/docusaurus-plugin-content-pages/index.js
+++ i18n/fr/docusaurus-plugin-content-pages/index.js
@@ -28,7 +28,7 @@ function HeroBanner() {
                             __html: translate({
                                 id: 'homepage.hero.title',
                                 message:
-                                    "Free</b> events and economic tools <b>creating</b> cooperative networks <b>cooperatifs</b>.",
+                                    "Outils <b>évènementiels</b> et économiques <b>libres</b> créateurs de réseaux <b>cooperatifs</b>.",
                                 description:
                                     'Titre',
                             }),
@@ -38,10 +38,10 @@ function HeroBanner() {
 
                 <div className={styles.indexCtas}>
                     <Link className="button button--primary" to="/docs/presentation/introduction">
-                        <Translate>To know more</Translate>
+                        <Translate>En savoir plus</Translate>
                     </Link>
                     <Link className="button button--info" to="/docs/presentation/demonstration">
-                        <Translate>Demonstration</Translate>
+                        <Translate>Démonstration</Translate>
                     </Link>
                 </div>
             </div>
@@ -56,7 +56,7 @@ export default function Home() {
     return (
         <Layout
             title={`${siteConfig.title}`}
-            description="Free event and economic tools creating cooperative networks: Cashless, ticketing, blockchain.">
+            description="Outils évènementiels et économiques libres créateurs de réseaux coopératifs: Cashless, billetterie, blockchain.">
             <main>
                 <HeroBanner/>
                 <HomepageFeatures/>
