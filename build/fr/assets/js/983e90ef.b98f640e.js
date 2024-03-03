c8cab2edf6613a267fd5f9ee3ff2ca84bcaaf208
Jonas Legion <jturbeaux@pm.me>
2024-03-03 11:06:55 +0100

    remove build and add social card

--- build/fr/assets/js/983e90ef.b98f640e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocumentation_v_2=self.webpackChunkdocumentation_v_2||[]).push([[3426],{4469:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
