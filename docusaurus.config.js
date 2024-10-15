f962df70c3d2fa418aa213adba77f59e36fd358b
achabran <95669774+achabran@users.noreply.github.com>
2024-10-15 22:01:19 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/TiBillet/documentation_v2/',
+                        'https://github.com/TiBillet/documentation_v2/tree/main',
                 },
                 blog: {
                     showReadingTime: true,
